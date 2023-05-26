1adacf13d31c8a0a7a526a37b9705d53f0771090
SunilKumarRoy450 <101394814+SunilKumarRoy450@users.noreply.github.com>
2023-05-26 15:00:59 +0530

    all routes completed

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,12 +1,67 @@
 const express = require("express");
-const router=express.Router();
-const User=require('../model/user.model');
+const router = express.Router();
+const User = require("../model/user.model");
 
-router.get('/',(req,res)=>{
-    res.send('Hello World!')
-})
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 //post
+router.post("/create", async (req, res) => {
+  try {
+    const { name, desc, assignTo } = req.body;
+    const user = new User({ name, desc, assignTo });
+    await user.save();
+    return res.status(201).send("User created successfully");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
 
+//get:id
+router.get("/get/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById({ _id: id });
+    return res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
 
-module.exports=router
+//delete
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete({ _id: id });
+    return res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
+//put
+router.put("/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, desc, assignTo } = req.body;
+    const user = await User.findByIdAndUpdate(
+      id,
+      {
+        name,
+        desc,
+        assignTo,
+      },
+      { new: true }
+    );
+    return res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
+module.exports = router;
